e3c6a259ab269d3673f8d73933535e0c7c914d1a
Clément Chaine <clement.chaine25@gmail.com>
2025-12-06 22:50:51 +0100

    Add prototype implementation

--- src/ecap5_dwbtimer.sv
+++ src/ecap5_dwbtimer.sv
@@ -21,6 +21,13 @@
  */
 
 module ecap5_dwbtimer #(
+  parameter int CLK_FREQ,
+
+  localparam logic TIMER_TIMEBASE_LOW   = 0,
+  localparam logic TIMER_TIMEBASE_HIGH  = 1,
+
+  /* 1kHz output frequency */
+  localparam int PRESCALER_LOAD = (CLK_FREQ / 1000) - 1
 )(
   input   logic         clk_i,
   input   logic         rst_i,
@@ -36,7 +43,75 @@ module ecap5_dwbtimer #(
   input   logic        wb_stb_i,
   output  logic        wb_ack_o,
   input   logic        wb_cyc_i,
-  output  logic        wb_stall_o,
+  output  logic        wb_stall_o
 );
+
+/*****************************************/
+/*           Internal signals            */
+/*****************************************/
+
+logic[31:0] mem_addr;
+logic       mem_read, mem_write;
+logic[31:0] mem_read_data_d, mem_read_data_q, 
+            mem_write_data;
+
+logic[31:0] prescaler_d, prescaler_q;
+
+/*****************************************/
+/*        Memory mapped registers        */
+/*****************************************/
+
+logic[63:0] timebase_q;
+
+/*****************************************/
+
+ecap5_dwbmmsc wb_interface_inst (
+  .clk_i (clk_i),   .rst_i (rst_i),
+  
+  .wb_adr_i (wb_adr_i),  .wb_dat_o (wb_dat_o),  .wb_dat_i   (wb_dat_i),
+  .wb_we_i  (wb_we_i),   .wb_sel_i (wb_sel_i),  .wb_stb_i   (wb_stb_i),
+  .wb_ack_o (wb_ack_o),  .wb_cyc_i (wb_cyc_i),  .wb_stall_o (wb_stall_o),
+
+  .addr_o       (mem_addr),
+  .read_o       (mem_read),
+  .read_data_i  (mem_read_data_q),
+  .write_o      (mem_write),
+  .write_data_o (mem_write_data),
+  .sel_o          ()
+);
+
+always_comb begin : register_access
+  // Set the data output for read requests
+  mem_read_data_d = 0;
+  case(mem_addr[2])
+    TIMER_TIMEBASE_LOW:  mem_read_data_d = timebase_q[31:0];
+    TIMER_TIMEBASE_HIGH: mem_read_data_d = timebase_q[63:32];
+    default:   mem_read_data_d = '0;
+  endcase
+end
+
+always_comb begin : prescaler
+  if(prescaler_q == '0) begin
+    prescaler_d = PRESCALER_LOAD;
+  end else begin
+    prescaler_d = prescaler_q - 1;
+  end
+end
+
+always_ff @(posedge clk_i) begin
+  if(rst_i) begin
+    prescaler_q <= PRESCALER_LOAD;
+    timebase_q <= '0;
+
+    mem_read_data_q <= '0;
+  end else begin
+    prescaler_q <= prescaler_d;
+    if(prescaler_q == '0) begin
+      timebase_q <= timebase_q + 1;
+    end
+
+    mem_read_data_q <= mem_read_data_d;
+  end
+end
 
 endmodule // ecap5_dwbtimer
